aabdee672f29849e6c6b92f0742d70506b9752bb
garretblankenship <garret.blankenship@coderacademy.edu.au>
2020-01-15 11:03:02 +1100

    Feature finished

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Garret from "./Garret";
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <h1>Hello class!</h1>
       <h2>This is the matrix</h2>
       <p>Hey there</p>
+      <Garret />
     </>
   );
 }
